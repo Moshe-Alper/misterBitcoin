55560078d1a443932be97cc9e02c7240d9f423ed
Moshik Alper <moshikalper@gmail.com>
2025-04-01 19:30:04 +0300

    Add contact details and statistic pages with initial layout

--- src/app/pages/contact-details-page/contact-details-page.component.ts
+++ src/app/pages/contact-details-page/contact-details-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { Observable } from 'rxjs';
+import { ContactService } from '../../../services/contact.service';
 
 @Component({
   selector: 'contact-details-page',
@@ -6,6 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './contact-details-page.component.html',
   styleUrl: './contact-details-page.component.scss'
 })
-export class ContactDetailsPageComponent {
+export class ContactDetailsPageComponent implements OnInit {
+
+  private contactService = inject(ContactService)
+  private destroyRef = inject(DestroyRef)
+
+  contacts: Contact[] | undefined
+  contacts$: Observable<Contact[]> = this.contactService.contacts$
+
+
+  ngOnInit() {
+
+  }
 
 }
