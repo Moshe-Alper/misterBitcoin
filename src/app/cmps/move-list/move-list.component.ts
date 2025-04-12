da22532af0ff9d1826d9e2242c7459683187c227
Moshik Alper <moshikalper@gmail.com>
2025-04-12 16:00:56 +0300

    Add move-list to details

--- src/app/cmps/move-list/move-list.component.ts
+++ src/app/cmps/move-list/move-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { Move } from '../../models/move.model';
 
 @Component({
   selector: 'move-list',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './move-list.component.scss'
 })
 export class MoveListComponent {
+
+  _contact!: Contact
+  title: string = ''
+
+  @Input() moves!: Move[]
+
+  @Input() set contact(contact: Contact | null) {
+    if (contact) {
+        const spaceIdx = contact.name.indexOf(' ')
+        const nameLength = contact.name.length
+        this.title = 'Your moves to ' + contact.name.slice(0, spaceIdx < 0 ? nameLength : spaceIdx)
+    } else {
+        this.title = 'Last 3 moves'
+    }
+    this._contact = contact || ({} as Contact)
+}
 
 }
--- src/app/pages/contact-details/contact-details.component.ts
+++ src/app/pages/contact-details/contact-details.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ContactService } from '../../../services/contact.service';
 import { Contact } from '../../models/contact.model';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map, Observable, switchMap } from 'rxjs';
+import { combineLatest, filter, map, Observable, switchMap } from 'rxjs';
 import { UserService } from '../../../services/user.service';
 import { User } from '../../models/user.modal';
 
@@ -22,8 +22,17 @@ export class ContactDetailsComponent  {
 
     
     contact$: Observable<Contact> = this.route.data.pipe(map(data => data['contact']))
+    user$: Observable<User> = this.userService.loggedInUser$.pipe(
+        filter((user): user is User => !!user) // Make sure we only get non-null users
+    )
     user: User = {} as User   
     
+    contactMoves$ = combineLatest([this.user$, this.contact$]).pipe(
+        filter(([user]) => !!user),
+        map(([user, contact]) => user.moves.filter(move => move.toId === contact._id)),
+    )
+
+
     onTransferCoins(amount: number) {
         this.contact$.pipe(
             switchMap(contact => this.userService.addMove(contact, amount)),
